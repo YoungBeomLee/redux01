f43fd08d4961bc3f5bd0a2df5242c19c71e43847
YoungBeomLee <edlee127127@gmail.com>
2023-03-24 11:54:46 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import {connect} from 'react-redux';
+import {incrementCounter,decrementCounter} from './actions'
+function App( {count,incrementCounter,decrementCounter}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={incrementCounter}>+</button>
+      <button onClick={decrementCounter}>-</button>
+      <button onClick={()=>console.log(count)}>Count: {count}</button>
+
     </div>
+    
   );
 }
-
-export default App;
+function mapStateToProps(state) {
+  return {
+    count:state.count,
+  };
+}
+//export default App;
+export default connect(mapStateToProps,{incrementCounter,decrementCounter})(App);
